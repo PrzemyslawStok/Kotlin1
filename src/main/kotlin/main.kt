ad5581ff7fbb41c343ba2b20c7ce16c055d15488
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-02 18:46:36 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,5 +6,8 @@ fun main(){
     println("Suma liczb $a i $b wynosi: $add")
     println("${a}+${b}=${a+b}")
 
+    println((1..10).map{a->a*a})
+    println((1..10).map{it*it})
 
+    println((1..10).map{it*it}.filter{ it<100 }.map{it*it*it})
 }
