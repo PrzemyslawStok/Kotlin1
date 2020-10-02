96a1d28e00a12b9eacdb5880fa29130e6fe0dfac
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-02 18:42:00 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,9 @@ fun main(){
     var a:Int = 2
     var b = 5
 
-    a = 10.0.toInt()
-
     val add = a+b
-    println("Suma liczb "+a+" i "+b+" wynosi: "+add)
-    println("${a}+${b}=${add}")
+    println("Suma liczb $a i $b wynosi: $add")
+    println("${a}+${b}=${a+b}")
+
+
 }
