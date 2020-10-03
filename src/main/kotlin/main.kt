6801b8c6145c24d992c825004d3768c0e5de6cc7
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-03 12:43:57 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,14 @@
 fun main(){
+    var a : Double = 2.0
+    var b = 5.0
+
+    b = 1.toDouble()
+
+    val const1 = 5.233434234
+    //const1 = 23433.0
+//a+b = 10
+    val add = a+b
+    println("Suma liczb "+a+" i "+b+" wynosi "+add)
+    println("Suma liczb ${a} i ${b} wynosi ${add}")
+    //2+5=10
 }
