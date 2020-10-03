b3744f516f8fef07d7010a3860a1bfabff463717
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-03 12:53:55 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,9 +6,14 @@ fun main(){
 
     val const1 = 5.233434234
     //const1 = 23433.0
-//a+b = 10
+    //a+b = 10
+
     val add = a+b
+    val sub = a-b
+
     println("Suma liczb "+a+" i "+b+" wynosi "+add)
     println("Suma liczb ${a} i ${b} wynosi ${add}")
+    println("${a}+${b}=${add}")
+    println("${a}-${b}=${sub}")
     //2+5=10
 }
