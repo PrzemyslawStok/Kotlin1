dd50a06c705510674325bf48f783cbd087256a9b
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-03 13:08:09 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,7 +8,7 @@ fun main(){
     //const1 = 23433.0
     //a+b = 10
 
-    val add = a+b
+    val add = add(a,b)
     val sub = a-b
 
     println("Suma liczb "+a+" i "+b+" wynosi "+add)
@@ -16,4 +16,15 @@ fun main(){
     println("${a}+${b}=${add}")
     println("${a}-${b}=${sub}")
     //2+5=10
+
+    var array = mutableListOf(1,2,3,4,5,6,7,8,9,10)
+
+    println(array.map{a->a*a}.filter{it%2==0})
+    println(array.map{2*it})
+
+    var array1 = (1..10).map{it*it}
+}
+
+fun add(a: Double, b: Double):Double{
+    return a+b;
 }
