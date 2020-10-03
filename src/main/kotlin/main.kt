5f85722891c7785644e4857bb3c4caad5a123e49
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-10-03 12:32:43 +0200

    cd..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,2 @@
 fun main(){
-    var a:Int = 2
-    var b = 5
-
-    val add = a+b
-    println("Suma liczb $a i $b wynosi: $add")
-    println("${a}+${b}=${a+b}")
-
-    println((1..10).map{a->a*a})
-    println((1..10).map{it*it})
-
-    println((1..10).map{it*it}.filter{ it<100 }.map{it*it*it})
-
-    val student = Student("Przemysław","Stokłosa")
-    student.printStudent()
 }
